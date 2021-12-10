d752bdfc609cb02c8dea8f6f7b880eafd135ef7c
unknown <Danrleysil46@gmail.com>
2021-12-10 20:55:09 -0300

    correcoes

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://ifpiparnaiba.edu.br/node/node",
-  baseURL: "https://ifpi-refeitorio.herokuapp.com/node",
+  baseURL: "http://ifpiparnaiba.edu.br/node/node",
+  // baseURL: "https://ifpi-refeitorio.herokuapp.com/node",
   // baseURL: "http://192.168.18.6:3334/node",
 });
 
